444d5347293b5bc5a3cc67fc2dcf64b5271d3ac1
Chris Cantu <christopher.cantu@gmail.com>
2012-01-30 00:01:55 -0600

    Added filter named query to Request object

--- grails-app/domain/com/rackspace/Request.groovy
+++ grails-app/domain/com/rackspace/Request.groovy
@@ -10,6 +10,16 @@ class Request {
 	Date dateCreated
 	Date lastUpdated
 
+	static namedQueries = {  
+		filter { params -> 
+			if ( params.type ) { eq 'type', params.type }
+			if ( params.subject ) { eq 'subject', params.subject }
+			if ( params.description ) { eq 'description', params.description }
+			if ( params.status ) { eq 'status', params.status }
+			if ( params.assigned ) { eq 'assigned', User.get(params.assigned) }
+		}
+	}
+
     static constraints = {
 		type blank:false
 		subject blank:false
