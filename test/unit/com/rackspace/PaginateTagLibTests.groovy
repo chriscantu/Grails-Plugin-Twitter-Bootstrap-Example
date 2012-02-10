d3204eb834bdb6821afb2a39528ee04eb2102484
Chris Cantu <christopher.cantu@gmail.com>
2012-02-09 23:48:39 -0600

    Added custom pagination to the app.

---
+++ test/unit/com/rackspace/PaginateTagLibTests.groovy
@@ -0,0 +1,17 @@
+package com.rackspace
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(PaginateTagLib)
+class PaginateTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
